008e76bdd44ab1da79561acc468c408e915a893d
Maxi-arch <mxmu025@gmail.com>
2024-10-04 12:53:43 +0300

    domcontent

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function(){
+   const change = this.getElementById("text")
+   change.textContent = 'This is really cool!'
+})
+console.log(change)
+
